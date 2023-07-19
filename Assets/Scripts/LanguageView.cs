ec97974fa04eeece94c2331f4eee5a12fb065100
FloofyProtogen <programmingfrog@gmail.com>
2023-07-19 15:54:47 -0400

    7/19

--- Assets/Scripts/LanguageView.cs
+++ Assets/Scripts/LanguageView.cs
@@ -13,14 +13,37 @@ public class LanguageView : MonoBehaviour
     [SerializeField]
     private Translator translator;
 
+
+    [SerializeField]
+    private Sprite offSprite;
+
+    [SerializeField]
+    private Sprite onSprite;
+
     private void Awake()
     {
-        for(int i = 0; i < System.Enum.GetValues(typeof(LanguageEnum.Languages)).Length; i++)
+        int selected = PlayerPrefs.GetInt("Language", 0);
+        translator.CurrentLanguage = (LanguageEnum.Languages)selected;
+        for (int i = 0; i < System.Enum.GetValues(typeof(LanguageEnum.Languages)).Length; i++)
         {
             Button button = Instantiate(languageSelectButtonPrefab, buttonParent);
             button.GetComponentInChildren<Text>().text = LanguageEnum.LanguageNames[i];
             int _i = i;
-            button.onClick.AddListener(() => translator.CurrentLanguage = (LanguageEnum.Languages)_i);
+            button.onClick.AddListener(() =>
+            {
+                translator.CurrentLanguage = (LanguageEnum.Languages)_i;
+                PlayerPrefs.SetInt("Language", _i);
+
+                foreach (Transform transform in buttonParent)
+                {
+                    transform.GetComponent<Image>().sprite = offSprite;
+                }
+
+                button.GetComponent<Image>().sprite = onSprite;
+
+            });
         }
+
+        buttonParent.GetChild(selected).GetComponent<Image>().sprite = onSprite;
     }
 }
